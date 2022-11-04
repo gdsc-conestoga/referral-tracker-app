233bd87c968439606916aea56c264c87862f2d30
decaf <siyang.he.uw@gmail.com>
2022-11-03 22:17:04 -0400

    centered button

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,54 @@ import 'package:referral_tracker/visuals.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const ReferralTrackerApp());
+void main() => runApp(MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const WelcomeScreen(),
+        '/login': (context) => const LoginScreen(),
+      },
+    ));
 
-class ReferralTrackerApp extends StatelessWidget {
-  const ReferralTrackerApp({Key? key}) : super(key: key);
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Referral Tracker App",
+        theme: ThemeData(
+          appBarTheme: trackerAppBarTheme,
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+                title: const Text(
+                  "Referral Tracker",
+                  style: trackerAppBarTextStyle,
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/login');
+                    },
+                    child: const Text("Log In", style: trackerLoginButtonStyle),
+                  ),
+                ]),
+            body: Center(
+                child: Container(
+              margin: const EdgeInsets.only(top: 200),
+              child: Column(children: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/login');
+                    },
+                    child: const Text("Apply for membership"))
+              ]),
+            ))));
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +61,21 @@ class ReferralTrackerApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text(
-            "Referral Tracker",
-            style: trackerAppBarTextStyle,
-          ),
+            title: const Text(
+              "Referral Tracker Second Screen",
+              style: trackerAppBarTextStyle,
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Cancel", style: trackerLoginButtonStyle),
+              ),
+            ]),
+        body: const Text(
+          "this is app content",
         ),
-        body: const Text("this is app content"),
       ),
     );
   }
--- lib/visuals.dart
+++ lib/visuals.dart
@@ -28,6 +28,20 @@ class CustomColours {
     800: Color.fromRGBO(187, 144, 70, .9),
     900: Color.fromRGBO(187, 144, 70, 1),
   });
+
+  static const MaterialColor conestogaWhite =
+      MaterialColor(0xFFFFFFFF, <int, Color>{
+    50: Color.fromRGBO(255, 255, 255, .1),
+    100: Color.fromRGBO(255, 255, 255, .2),
+    200: Color.fromRGBO(255, 255, 255, .3),
+    300: Color.fromRGBO(255, 255, 255, .4),
+    400: Color.fromRGBO(255, 255, 255, .5),
+    500: Color.fromRGBO(255, 255, 255, .6),
+    600: Color.fromRGBO(255, 255, 255, .7),
+    700: Color.fromRGBO(255, 255, 255, .8),
+    800: Color.fromRGBO(255, 255, 255, .9),
+    900: Color.fromRGBO(255, 255, 255, 1),
+  });
 }
 
 const AppBarTheme trackerAppBarTheme = AppBarTheme(
@@ -37,5 +51,11 @@ const AppBarTheme trackerAppBarTheme = AppBarTheme(
 const TextStyle trackerAppBarTextStyle = TextStyle(
   color: CustomColours.conestogaGold,
   fontSize: 30.0,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle trackerLoginButtonStyle = TextStyle(
+  color: CustomColours.conestogaWhite,
+  fontSize: 20.0,
   fontWeight: FontWeight.bold,
 );
