c169e06f3b11de66ffc2bee71e7d2da8e1e06bdf
decaf <siyang.he.uw@gmail.com>
2022-12-04 18:53:14 -0500

    fix _userLookup() in class DatabaseService

--- lib/screens/membership_application_screen.dart
+++ lib/screens/membership_application_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:referral_tracker/main.dart';
--- lib/utils/database_service.dart
+++ lib/utils/database_service.dart
@@ -4,9 +4,11 @@ class DatabaseService {
   Future<DocumentSnapshot?> _userLookup(String studentNumber) async {
     final CollectionReference users =
         FirebaseFirestore.instance.collection('users');
-    final query =
-        users.where('student_number', isEqualTo: studentNumber).limit(1);
-    return await query.get().then((querySnapShot) => querySnapShot.docs[0]);
+    final QuerySnapshot query = await users
+        .where('student_number', isEqualTo: studentNumber)
+        .limit(1)
+        .get();
+    return query.size > 0 ? query.docs[0] : null;
   }
 
   Future<bool> addUser({
@@ -15,11 +17,12 @@ class DatabaseService {
     required String studentNumber,
     int totalCoins = 5,
   }) async {
+    bool userIsAdded = true;
     try {
       CollectionReference users =
           FirebaseFirestore.instance.collection('users');
 
-      return await users
+      userIsAdded = await users
           .add({
             'username': username,
             'email': email,
@@ -29,50 +32,60 @@ class DatabaseService {
           .then((documentSnapshot) => true)
           .catchError((error) => false);
     } catch (e) {
-      return false;
+      userIsAdded = false;
     }
+
+    return userIsAdded;
   }
 
   Future<bool> removeUser(String studentNumber) async {
+    bool userIsRemoved = true;
     try {
       final DocumentSnapshot? userDocument = await _userLookup(studentNumber);
-      return userDocument == null
+      userIsRemoved = (userDocument == null)
           ? false
           : await userDocument.reference
               .delete()
               .then((value) => true)
               .catchError((error) => false);
     } catch (e) {
-      return false;
+      userIsRemoved = false;
     }
+
+    return userIsRemoved;
   }
 
   Future<bool?> hasUser(String studentNumber) async {
+    bool? userExists = true;
     try {
-      return await _userLookup(studentNumber) != null;
+      userExists = (await _userLookup(studentNumber) != null);
     } catch (e) {
-      return null;
+      userExists = null;
     }
+    return userExists;
   }
 
   Future<bool> addReferralBonus(String referrerStudentNumber, int bonus) async {
+    bool bonusIsAdded = true;
     try {
       final DocumentSnapshot? referrerDocument =
           await _userLookup(referrerStudentNumber);
 
       if (referrerDocument == null) {
-        return false;
+        bonusIsAdded = false;
+      } else {
+        final int currCoinCount = referrerDocument.get('total_coins');
+        bonusIsAdded = await referrerDocument.reference
+            .update({
+              'total_coins': currCoinCount + bonus,
+            })
+            .then((value) => true)
+            .catchError((error) => false);
       }
-
-      final int currCoinCount = referrerDocument.get('total_coins');
-      return await referrerDocument.reference
-          .update({
-            'total_coins': currCoinCount + bonus,
-          })
-          .then((value) => true)
-          .catchError((error) => false);
     } catch (e) {
-      return false;
+      bonusIsAdded = false;
     }
+
+    return bonusIsAdded;
   }
 }
